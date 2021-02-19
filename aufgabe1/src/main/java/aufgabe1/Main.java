1399e103a40c70be58d977f387646da250aa41be
alex9849 <alexander@liggesmeyer.net>
2021-02-19 15:29:56 +0100

    Fixed Aufgabe 1

--- aufgabe1/src/main/java/aufgabe1/Main.java
+++ aufgabe1/src/main/java/aufgabe1/Main.java
@@ -1,14 +1,23 @@
 package aufgabe1;
 
-import java.util.concurrent.TimeUnit;
 import util.Tools;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+    private static volatile boolean running = true;
+
     public static void main(String... args) {
-        while (!Thread.currentThread().isInterrupted()) {
-            System.out.println("Hello world");
+
+        new Thread(() -> {
             Tools.sleep(3, TimeUnit.SECONDS);
+            running = false;
+        }).start();
+
+        while (running) {
+            System.out.println("Hello world");
+            Tools.sleep(500, TimeUnit.MILLISECONDS);
         }
     }
 
